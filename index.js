97dfcda640a22b91d55180afe483fb638cf814bf
giga0909 <gchagalidze0109@gmail.com>
2023-11-13 22:17:29 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const GelasAge= 34
+
+if(GelasAge >= 18){
+    console.log(`სრულწლოვანია`)
+}else if(age < 18){ console.log(`არ არის სრულწლოვანი`)
+
+}
+
+const WeekDay=4
+
+switch(WeekDay) {
+
+    case 1: 
+    console.log(`orshabati`)
+    break;
+
+    case 2:
+    console.log(`samshabati`)
+    break;
+
+    case 3:
+        console.log(`otxshabati`)
+        break;
+
+        case 4:
+            console.log(`xutshabati`)
+            break;
+
+            case 5:
+              console.log(`paraskevi`)
+              break;
+
+              case 6:
+                console.log(`shabati`)
+                break;
+
+                case 7:
+                    console.log(`kvira`)
+                    break;
+
+
+}
+
+
+
+
+
+
